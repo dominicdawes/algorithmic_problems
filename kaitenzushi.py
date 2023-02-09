0adb34ee126484c8e3a4a05ad830be313dee7dd0
dominicdawes <78048741+dominicdawes@users.noreply.github.com>
2023-02-09 09:25:04 -0500

    Update kaitenzushi.py
    Faster TC with hashing

--- kaitenzushi.py
+++ kaitenzushi.py
@@ -7,24 +7,42 @@ from queue import Queue
 # Write any import statements here
 
 def getMaximumEatenDishCount(N: int, D: List[int], K: int) -> int:
-  # Write your code here
-  q = []  # create queue from array
+  # Write your code here ( faster time complexity )
+  Sk = set([])  # create hashable obj
+  Qk = deque([])
   eaten = 0  # create coujnter
   
   for sushi in D:
     # Check already eaten
-    if sushi in q:
-      print("no opp")
-    else:
+    if sushi not in Sk:
+      if len(Sk) == K:
+        # rm old dish
+        sushi_old = Qk.popleft()
+        Sk.remove(sushi_old)
+      # else add to memory
+      Sk.add(sushi)
+      Qk.append(sushi)
+      print("added")
       eaten+=1
-      # Cache what is eaten
-      if len(q) < K:
-        # fill queue put()
-        q.append(sushi)
-      else:
-        # pop out First-Out
-        rm = q.pop(0)
-        # append Next-In
-        q.append(sushi)
+      
+#   Write your code here ( faster time complexity )     
+#   q = []  # create queue from array
+#   eaten = 0  # create coujnter
+  
+#   for sushi in D:
+#     # Check already eaten
+#     if sushi in q:
+#       print("no opp")
+#     else:
+#       eaten+=1
+#       # Cache what is eaten
+#       if len(q) < K:
+#         # fill queue put()
+#         q.append(sushi)
+#       else:
+#         # pop out First-Out
+#         rm = q.pop(0)
+#         # append Next-In
+#         q.append(sushi)
   
   return eaten
