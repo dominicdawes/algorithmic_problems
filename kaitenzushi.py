ee08bfa02c44526b39ecdc3f26b287d83cc6176b
dominicdawes <78048741+dominicdawes@users.noreply.github.com>
2023-02-09 10:22:28 -0500

    Update kaitenzushi.py

--- kaitenzushi.py
+++ kaitenzushi.py
@@ -8,26 +8,25 @@ from queue import Queue
 
 def getMaximumEatenDishCount(N: int, D: List[int], K: int) -> int:
   # Write your code here ( faster time complexity )
-  Sk = set([])  # create hashable obj
-  Qk = deque([])
-  eaten = 0  # create coujnter
+  Sk = set([])  # create hash set obj
+  Qk = deque([]) # Doubly Ended Queue
+  eaten = 0  # create counter
   
   for sushi in D:
     # Check already eaten
     if sushi not in Sk:
       if len(Sk) == K:
-        # rm old dish
         sushi_old = Qk.popleft()
-        Sk.remove(sushi_old)
+        Sk.remove(sushi_old) # rm old sushi from the set
       # else add to memory
       Sk.add(sushi)
-      Qk.append(sushi)
+      Qk.append(sushi)  # inserts at right end of the deque
       print("added")
       eaten+=1
       
 #   Write your code here ( faster time complexity )     
 #   q = []  # create queue from array
-#   eaten = 0  # create coujnter
+#   eaten = 0  # create counter
   
 #   for sushi in D:
 #     # Check already eaten
