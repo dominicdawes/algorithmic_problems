35ac5b1a260657a5064284053b215a2db6896631
dominicdawes <78048741+dominicdawes@users.noreply.github.com>
2023-02-14 11:54:58 -0500

    Update hops.py
    additional solution

--- hops.py
+++ hops.py
@@ -14,3 +14,12 @@ def getSecondsRequired(N: int, F: int, P: List[int]) -> int:
 
   # hopping over any other frogs on intermediate lily pads along the way
   return N - min(P)
+
+#   This also works however I need to review why
+#   minimum = N
+  
+#   for i in range(0, F):
+#     if P[i] < minimum:
+#       minimum = P[i]
+
+#   return N - minimum
